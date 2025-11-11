ed180606ed8a517684247529db05120e0bea1206
Nambwa04 <nambwaberyl2018@gmail.com>
2025-11-11 21:38:04 +0300

    Remove commented-out code and ensure CORS_ALLOWED_ORIGINS is properly defined for frontend access

--- backend/telemed/deployment.py
+++ backend/telemed/deployment.py
@@ -27,11 +27,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-'''
-CORS_ALLOWED_ORIGINS = [
-]
-'''
 
+CORS_ALLOWED_ORIGINS = [
+    "https://telemedplusfrontend.z1.web.core.windows.net/"
+]
 
 STORAGES = {
     "default": {
